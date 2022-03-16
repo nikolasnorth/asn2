de72d5cc817a216d0b36eeeea98c3043b9e63820
Nikolas North <nikolasn09@gmail.com>
2022-03-16 14:26:23 -0400

    initial commit

---
+++ mapper.py
@@ -0,0 +1,17 @@
+"""
+`mapper.py` reads data from STDIN, splits it into words, and outputs a list of lines mapping words to their
+(intermediate) counts to STDOUT. However, it will not compute a sum of the word's occurrences. The reducer script will
+do the final sum count.
+"""
+
+import sys
+
+
+for line in sys.stdin:
+    # Remove leading and trailing whitespace
+    line = line.strip()
+    # Split the line into words
+    words = line.split()
+    # Increase counters
+    for word in words:
+        print(f"{word}\t1")
---
+++ reducer.py
@@ -0,0 +1,34 @@
+"""
+`reducer.py` reads the output of `mapper.py` from STDIN and sums the occurrences of every word. The counts are written
+to STDOUT.
+"""
+
+import sys
+
+current_word = ""
+current_count = 0
+word = ""
+
+for line in sys.stdin:
+    # Remove leading and trailing whitespace
+    line = line.strip()
+    # Parse the current line
+    word, count = line.split("\t")
+    try:
+        count = int(count)
+    except ValueError:
+        # Skip
+        continue
+    # Since Hadoop sorts map output by key (i.e. `word`) before it is passed to the reducer, we are able to use an
+    # if condition here.
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            # Write result to STDOUT
+            print(f"{current_word}\t{current_count}")
+        current_word = word
+        current_count = count
+# Output the last word
+if current_word == word:
+    print(f"{current_word}\t{current_count}")
